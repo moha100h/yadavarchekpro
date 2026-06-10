67a2a72f4ea4dc3c20a0e5d9557a53a79dbf9816
moha100h <mmlu.tel@gmail.com>
2026-06-10 18:01:16 +0330

    feat: RepositoryModule.kt

--- app/src/main/java/com/yadavarcheck/tisa/di/RepositoryModule.kt
+++ app/src/main/java/com/yadavarcheck/tisa/di/RepositoryModule.kt
@@ -1,17 +1,16 @@
 package com.yadavarcheck.tisa.di
-
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import com.yadavarcheck.tisa.data.repository.*
-import com.yadavarcheck.tisa.domain.repository.*
-import javax.inject.Singleton
+import com.yadavarcheck.tisa.data.repository.CheckRepositoryImpl
+import com.yadavarcheck.tisa.data.repository.CustomerRepositoryImpl
+import com.yadavarcheck.tisa.data.repository.NotificationRepositoryImpl
+import com.yadavarcheck.tisa.domain.repository.CheckRepository
+import com.yadavarcheck.tisa.domain.repository.CustomerRepository
+import com.yadavarcheck.tisa.domain.repository.NotificationRepository
+import dagger.Binds; import dagger.Module; import dagger.hilt.InstallIn; import dagger.hilt.components.SingletonComponent; import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-    @Binds @Singleton abstract fun bindCheckRepo(impl: CheckRepositoryImpl): CheckRepository
-    @Binds @Singleton abstract fun bindCustomerRepo(impl: CustomerRepositoryImpl): CustomerRepository
-    @Binds @Singleton abstract fun bindNotifRepo(impl: NotificationRepositoryImpl): NotificationRepository
+    @Binds @Singleton abstract fun bindCheckRepository(impl: CheckRepositoryImpl): CheckRepository
+    @Binds @Singleton abstract fun bindNotificationRepository(impl: NotificationRepositoryImpl): NotificationRepository
+    @Binds @Singleton abstract fun bindCustomerRepository(impl: CustomerRepositoryImpl): CustomerRepository
 }
