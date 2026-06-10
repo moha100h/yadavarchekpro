0beb961e9411af98b0096cab66cf6af4f6b31d35
moha100h <mmlu.tel@gmail.com>
2026-06-10 14:25:47 +0330

    feat: OnboardingViewModel

---
+++ app/src/main/java/com/yadavarcheck/tisa/ui/screens/onboarding/OnboardingViewModel.kt
@@ -0,0 +1,13 @@
+package com.yadavarcheck.tisa.ui.screens.onboarding
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import com.yadavarcheck.tisa.data.preferences.PreferencesManager
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardingViewModel @Inject constructor(private val prefs: PreferencesManager) : ViewModel() {
+    fun setOnboarded() = viewModelScope.launch { prefs.setOnboarded(true) }
+}
