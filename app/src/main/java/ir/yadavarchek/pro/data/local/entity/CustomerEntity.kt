0757f77cec9392decf54e819669921bddbd28adb
moha100h <mmlu.tel@gmail.com>
2026-06-10 13:03:13 +0330

    feat: data — CustomerEntity

---
+++ app/src/main/java/ir/yadavarchek/pro/data/local/entity/CustomerEntity.kt
@@ -0,0 +1,15 @@
+package ir.yadavarchek.pro.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "customers")
+data class CustomerEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String = "",
+    val mobile: String = "",
+    val phone: String = "",
+    val address: String = "",
+    val notes: String = "",
+    val createdAt: Long = System.currentTimeMillis()
+)
